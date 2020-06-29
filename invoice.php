88e7c129cefa45ccf280883eae79cbe2526fd8ca
ohfaiz <67558640+ohfaiz@users.noreply.github.com>
2020-06-29 20:59:59 +0800

    update invoice.php

--- invoice.php
+++ invoice.php
@@ -323,13 +323,32 @@ if(isset($_SESSION["email"])){
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
 
-                    <button type="button" id="sidebarCollapse" class="btn btnToggle">
+                    <button type="button" id="sidebarCollapse" class="btn btn-info">
                         <i class="fas fa-align-left"></i>
+                        
                     </button>
-                    <a href="logout.php" class="btn btn-warning">
+                    <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fas fa-align-justify"></i>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="nav navbar-nav ml-auto">
+                            <li class="nav-item active">
+                                <a href="change.php" class="btn">
+                        <span class="fa fa-unlock-alt" title="Change Password"></span>
+                            </li>
+                            <li class="nav-item">
+                                <a href="question.php" class="btn">
+                        <span class="fa fa-book" title="Security Question"></span>
+                            </li>
+                         
+                            <li class="nav-item">
+                                 <a href="logout.php" class="btn">
                         <span class="fas fa-sign-out-alt" title="Logout"></span>
                     </a>
-
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
             
