000d454ca878f1c3919a06b312504c712dcff441
ohfaiz <67558640+ohfaiz@users.noreply.github.com>
2020-06-29 20:29:31 +0800

    update invoice.php

--- invoice.php
+++ invoice.php
@@ -954,6 +954,20 @@ return true;
 }
 });
 </script>
+	  <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+<!-- Custom styles for this page -->
+  <link href="js-datatable/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+    <!-- Page level plugins -->
+    <script src="js-datatable/jquery.dataTables.min.js"></script>
+    <script src="js-datatable/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="js-datatable/datatables-demo.js"></script>
 	
 	
 	 <script type="text/javascript">
