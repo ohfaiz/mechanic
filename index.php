f107386be2f6c69e82bb7e1b2757c0fde5d659a1
ohfaiz <67558640+ohfaiz@users.noreply.github.com>
2020-06-29 19:43:17 +0800

    update index.php

--- index.php
+++ index.php
@@ -76,7 +76,7 @@
                             </li>
                          
                             <li class="nav-item">
-                                 <a href="logout.php" class="btn btn-dark">
+                                 <a href="logout.php" class="btn">
                         <span class="fas fa-sign-out-alt" title="Logout"></span>
                     </a>
                             </li>
