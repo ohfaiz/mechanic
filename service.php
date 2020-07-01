72f1e1570d4fa92a5068776563be18fc898cf29c
ohfaiz <67558640+ohfaiz@users.noreply.github.com>
2020-07-02 06:12:27 +0800

    Update service.php

--- service.php
+++ service.php
@@ -169,14 +169,24 @@ if(isset($_SESSION["email"])){
 	  
         </div>
     </div>
-	<!-- jQuery CDN - Slim version (=without AJAX) -->
+	 <!-- jQuery CDN - Slim version (=without AJAX) -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 
     <!-- Popper.JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-<!-- Custom styles for this page -->
-  <link href="js-datatable/dataTables.bootstrap4.min.css" rel="stylesheet">
 
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+    <!-- Custom styles for this page -->
+    <link href="js-datatable/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+    <!-- Page level plugins -->
+    <script src="js-datatable/jquery.dataTables.min.js"></script>
+    <script src="js-datatable/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="js-datatable/datatables-demo.js"></script>
     
 
    
