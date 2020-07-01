bb7e27130b1343cc241c662c0fac9fbd7a3497d7
ohfaiz <67558640+ohfaiz@users.noreply.github.com>
2020-07-02 03:33:34 +0800

    Update customer.php

--- customer.php
+++ customer.php
@@ -187,12 +187,7 @@
 <!-- Custom styles for this page -->
   <link href="js-datatable/dataTables.bootstrap4.min.css" rel="stylesheet">
 
-    <!-- Page level plugins -->
-    <script src="js-datatable/jquery.dataTables.min.js"></script>
-    <script src="js-datatable/dataTables.bootstrap4.min.js"></script>
-
-    <!-- Page level custom scripts -->
-    <script src="js-datatable/datatables-demo.js"></script>
+   
 	
 	 <script type="text/javascript">
         $(document).ready(function () {
