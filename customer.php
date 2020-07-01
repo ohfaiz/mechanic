c919083331696d1c2cdad3d4ab3f56fa15c6e80e
ohfaiz <67558640+ohfaiz@users.noreply.github.com>
2020-07-02 06:21:22 +0800

    Update customer.php

--- customer.php
+++ customer.php
@@ -129,17 +129,16 @@
                         <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                           <thead>
                             <tr>
-            <th>No.</th>
-			<th>Date Register</th>
-            <th>Customer Name</th>
-			<th>Phone Number</th>
+							<th>No.</th>
+							<th>Date Register</th>
+							<th>Customer Name</th>
+							<th>Phone Number</th>
   		
             <th>Action</th>  
                             </tr>
                           </thead>
-                         
                           <tbody>
-                            <?php
+                           <?php
                                 $sql = "SELECT * FROM customer";
           $result = mysqli_query($con,$sql);
           $x = 1;
@@ -147,28 +146,22 @@
           {
           while($row = mysqli_fetch_array($result))
           {
+                            
                             ?>
 
-                            
-          <tr>
-            <td><?php echo $x++ ?></td>
-			<td><?php echo $row['date_register'];?></td>
-           <td><?php echo $row['Fullname'];?></td>
-		 
-		   
-            <form action="" method="post">
-			  <td><input type="number" name="phone" min="0" value="<?php echo $row['phone'];?>"</td>
-            <input name="CustomerId" autofocus required class="input" type="hidden" value="<?php echo $row['CustomerId']; ?>">
-            
-            
-            <td><input type="submit" name="update" value="Update"></td>
-          </tr>
-          </form>
-          <?php
-        }
-          }
-        ?>                          
-                          </tbody>
+                            <tr>
+                              <form action="" method="post">
+                              <td><input type="hidden" name="CustomerId" value="<?php echo $row['CustomerId'];?>"><?php echo $row['Fullname'];?> </td>
+                              <td><?php echo $row['date_register'];?></td>
+                              <td><input type="number" name="phone" min="0" value="<?php echo $row['phone'];?>"</td>
+                              <td><input type="submit" name="update" value="Update"></td>  
+                              </form> 
+                            </tr> 
+                            <?php
+                              }
+                              }
+                            ?>   
+                          </tbody>                         
                         </table>
                       </div>
                     </div>
@@ -179,13 +172,24 @@
         </div>
     </div>
 
-    <!-- jQuery CDN - Slim version (=without AJAX) -->
+   	 <!-- jQuery CDN - Slim version (=without AJAX) -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 
     <!-- Popper.JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-<!-- Custom styles for this page -->
-  <link href="js-datatable/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+    <!-- Custom styles for this page -->
+    <link href="js-datatable/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+    <!-- Page level plugins -->
+    <script src="js-datatable/jquery.dataTables.min.js"></script>
+    <script src="js-datatable/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="js-datatable/datatables-demo.js"></script>
 
    
 	
